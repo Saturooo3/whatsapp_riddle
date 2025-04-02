1dd9dfceb531324a7d35b5fe2419ad34742dc48e
Diego Havenstein <diego@ww-lab.de>
2025-04-02 09:53:24 +0200

    Example usage of openai message history with structured outputs

--- tests/test_openai_tool.py
+++ tests/test_openai_tool.py
@@ -1,6 +1,6 @@
 import pytest
 from openai_tool import OpenAITool
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 def test_openai_tool():
     tool = OpenAITool()
@@ -12,6 +12,9 @@ def test_openai_tool():
 class CountryStats(BaseModel):
     capital_city: str
     population: int
+    def dump(self)->str:
+        return f"Capital city is {self.capital_city}, population is {self.population}"
+
 
 
 def test_openai_with_structure():
@@ -22,6 +25,12 @@ def test_openai_with_structure():
     response = tool.structured_answer(messages, CountryStats)
     print(response)
 
+class PopulationStats(BaseModel):
+    average_age: float = Field(description="The average age of the people in the city")
+    languages_spoken: list[str] = Field(description="The languages spoken in the city, ordered by most speakers")
+    def dump(self)->str:
+        return f"Average age is {self.average_age}, languages spoken are {', '.join(self.languages_spoken)}"
+
 
 def test_openai_with_history():
     tool = OpenAITool()
@@ -30,8 +39,14 @@ def test_openai_with_history():
         {"role": "user", "content": "Was ist die Hauptstadt und die Einwohnerzahl von Deutschland?"}
     ]
 
-    response = tool.structured_answer(messages, CountryStats)
-    print(response)
+    first_response : CountryStats = tool.structured_answer(messages, CountryStats)
+    print(first_response.dump())
+    messages.append({"role": "assistant", "content": first_response.dump()})
+    messages.append({"role" : "user", "content": "What is the average age of people in the city, and what are the most spoken languages?"})
+
+    second_response : PopulationStats = tool.structured_answer(messages, PopulationStats)
+    print(second_response.dump())
+
 
 
 
